b63303e891cb5a26891657bea75b78a19c23457d
AZEN-SGG <rascatov.alex@gmail.com>
2026-03-06 09:04:07 +0300

    HW is done!?

---
+++ 2026.03.06/dist/Krivoruchenko_SK/command.h
@@ -0,0 +1,302 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#include "condition.h"
+#include "operation.h"
+#include "record.h"
+#include "pattern.h"
+
+#include <cstdio>
+
+class request;
+
+enum class fields_t
+{
+	name,
+	phone,
+	group,
+};
+
+
+class command : public record, public pattern
+{
+	private:
+		condition c_name	= condition::none;
+		condition c_phone	= condition::none;
+		condition c_group	= condition::none;
+		operation op		= operation::none;
+	public:
+		command () = default;
+		~command () = default;
+
+		command& operator= (command&& x)
+		{
+			if (this == &x)
+				return (*this);
+
+			word = std::move(x.word);
+			span = std::move(x.span);
+			spec = std::move(x.spec);
+			group = x.group;
+			phone = x.phone;
+			
+			c_name = x.c_name;
+			c_phone = x.c_phone;
+			c_group = x.c_group;
+			op = x.op;
+
+			x.erase();
+
+			return (*this);
+		}
+		command& operator= (const command& x) = delete;
+
+		bool parse (char *string)
+		{
+			erase();
+
+			char field[LEN] = {}, negation[LEN] = {}, oper[LEN] = {}, value[LEN] = {};
+			int n = sscanf(string, "%s%s%s%s", field, negation, oper, value);
+			if (n != 3 && n != 4)
+				return false;
+
+			if ((n == 4) && std::strncmp(negation, "not", 4) == 0)
+			{
+				if (std::strncmp(oper, "like", 5) == 0)
+				{
+					if (std::strncmp(field, "name", 5) != 0)
+						return false;
+
+					if (read_word(value) != io_status::success)
+						return false;
+
+					c_name = condition::nlike;
+					
+					return true;
+				} else
+					return false;
+			} else if (n == 4)
+				return false;
+
+			switch (field[0])
+			{
+				case 'n':
+					return parse_name(field, negation, oper);
+				case 'p':
+					return parse_phone(field, negation, oper);
+				case 'g':
+					return parse_group(field, negation, oper);
+				default:
+					return false;
+			}
+		}
+
+		bool parse_name (char *field, char *oper, char *value)
+		{
+			if (std::strncmp(field, "name", 5) != 0)
+				return false;
+
+			if (!parse_condition(oper, fields_t::name))
+				return false;
+
+			switch (c_name)
+			{
+				case condition::none:
+					return false;
+				case condition::like:
+					if (read_word(value) != io_status::success)
+						return false;
+					break;
+				default:
+					init(value, 0, 0);
+			}
+
+			return true;
+		}
+
+		bool parse_phone (char *field, char *oper, char *value)
+		{
+			if (std::strncmp(field, "phone", 6) != 0)
+				return false;
+
+			if (!parse_condition(oper, fields_t::phone))
+				return false;
+
+			bool res;
+			switch (c_phone)
+			{
+				case condition::none:
+					res = false;
+					break;
+				case condition::like:
+					res = false;
+					break;
+				default:
+					res = (sscanf(value, "%d", &phone) == 1);
+					break;
+			}
+
+			return res;
+		}
+
+		bool parse_group (char *field, char *oper, char *value)
+		{
+			if (std::strncmp(field, "group", 6) != 0)
+				return false;
+
+			if (!parse_condition(oper, fields_t::group))
+				return false;
+
+			bool res;
+			switch (c_group)
+			{
+				case condition::none:
+					res = false;
+					break;
+				case condition::like:
+					res = false;
+					break;
+				default:
+					res = (sscanf(value, "%d", &group) == 1);
+					break;
+			}
+
+			return res;
+		}
+
+		bool parse_condition (const char *oper, const fields_t field)
+		{
+			condition cond = condition::none;
+
+			if (oper[1] == '\0')
+			{
+				switch (oper[0])
+				{
+					case '<':
+						cond = condition::lt;
+						break;
+					case '>':
+						cond = condition::gt;
+						break;
+					case '=':
+						cond = condition::eq;
+						break;
+					default:
+						return false;
+				}
+			} else if (oper[1] == '=')
+			{
+				if (oper[0] == '<')
+						cond = condition::le;
+				else if (oper[0] == '>')
+						cond = condition::ge;
+				else
+					return false;
+			} else if (oper[0] == '<' && oper[1] == '>')
+				cond = condition::ne;
+			else if (std::strncmp(oper, "like", 5) == 0)
+				cond = condition::like;
+			else
+				return false;
+
+			switch (field)
+			{
+				case fields_t::name:
+					c_name = cond;
+					break;
+				case fields_t::phone:
+					c_phone = cond;
+					break;
+				case fields_t::group:
+					c_group = cond;
+					break;
+			}
+
+			return true;
+		}
+
+		void print (FILE *fp = stdout) const
+		{
+			condition cond = condition::none;
+			if (c_group != condition::none)
+			{
+				fprintf(fp, "%d", group);
+				cond = c_group;
+			} else if (c_phone != condition::none)
+			{
+				fprintf(fp, "%d", phone);
+				cond = c_phone;
+			} else if (c_name != condition::none)
+			{
+				fprintf(fp, "%s", word.get());
+				cond = c_name;
+			} else
+				fprintf(fp, "none");
+
+			switch (cond)
+			{
+				case condition::none:
+					fprintf(fp, " none\n\n");
+					break;
+				case condition::lt:
+					fprintf(fp, " <\n\n");
+					break;
+				case condition::gt:
+					fprintf(fp, " >\n\n");
+					break;
+				case condition::le:
+					fprintf(fp, " <=\n\n");
+					break;
+				case condition::ge:
+					fprintf(fp, " >=\n\n");
+					break;
+				case condition::eq:
+					fprintf(fp, " =\n\n");
+					break;
+				case condition::ne:
+					fprintf(fp, " <>\n\n");
+					break;
+				case condition::like:
+					fprintf(fp, " like\n\n");
+					break;
+				case condition::nlike:
+					fprintf(fp, " not like\n\n");
+					break;
+			}
+		}
+
+		bool apply (const record& x) const
+		{
+			bool res = false;
+			if (c_group != condition::none)
+				res = x.compare_group(c_group, *this);
+			else if (c_phone != condition::none)
+				res = x.compare_phone(c_phone, *this);
+			else if (c_name != condition::none)
+			{
+				if (c_name == condition::like)
+					res = is_valid(x.get_word(), 0, 0);
+				else if (c_name == condition::nlike)
+					res = !is_valid(x.get_word(), 0, 0);
+				else
+					res = x.compare_word(c_name, *this);
+			}
+
+			return res;
+		}
+
+		friend class request;
+	protected:
+		void erase ()
+		{
+			record::erase();
+			pattern::erase();
+
+			c_name 	= condition::none;
+			c_phone	= condition::none;
+			c_group	= condition::none;
+			op = operation::none;
+		}
+};
+
+#endif // COMMAND_H
---
+++ 2026.03.06/dist/Krivoruchenko_SK/list.h
@@ -0,0 +1,164 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include "io_status.h"
+#include "list_node.h"
+#include "request.h"
+
+#include <cstdio>
+#include <new>
+
+template <typename T>
+class list2
+{
+	private:
+		list2_node<T> * head = nullptr;
+		static unsigned int r;
+	public:
+		list2 () = default;
+		~list2 () { erase (); }
+		
+		list2 (const list2 &) = delete;
+		list2 (list2 && r)
+		{
+			head = r.head;
+			r.head = nullptr;
+		}
+
+		list2 & operator= (const list2 &) = delete;
+		list2 & operator= (list2 && r)
+		{
+			if (this == &r)
+				return *this;
+			
+			erase();
+			
+			head = r.head;
+			r.head = nullptr;
+			
+			return *this;
+		}
+
+		static void set_r (int r) { list2<T>::r = r; };
+
+		io_status read (FILE *fp = stdin, const unsigned int max_read = -1)
+		{
+			list2_node<T> buf,
+				*prev = nullptr,
+				*next = nullptr;
+			io_status ret;
+
+			erase();
+
+			if ((ret = buf.read(fp)) != io_status::success)
+				return ret;
+
+			head = new list2_node<T>;
+			if (head == nullptr)
+				return io_status::memory;
+
+			*head = (list2_node<T> &&)buf;
+			list2_node<T> *curr = head;
+	
+			unsigned int i = 1;
+			while ((i < max_read) && (buf.read(fp) == io_status::success))
+			{
+				next = new list2_node<T>;
+	
+				if (!next)
+				{
+					erase();
+					return io_status::memory;
+				}
+	
+				*next = (list2_node<T> &&)buf;
+		
+				curr->prev = prev;
+				curr->next = next;
+		
+				prev = curr;
+				curr = next;
+
+				i++;
+			} if ((!feof(fp)) && (i < max_read))
+			{
+				erase();
+				return io_status::format;
+			}
+
+			curr->prev = prev;
+			curr->next = nullptr;
+
+			return io_status::success;
+		}
+
+		io_status read_file (char *filename, const unsigned int max_read = -1)
+		{
+			FILE *fp = fopen(filename, "r");
+			if (fp == nullptr)
+				return io_status::open;
+
+			io_status ret = read(fp, max_read);
+
+			fclose(fp);
+			return ret;
+		}
+
+		void print (FILE *fp = stdout) const
+		{
+			unsigned int i = 0;
+			for (const list2_node<T> *curr = head ; (curr && (i < r)) ; curr = curr->next)
+			{
+				curr->print(fp);
+				i++;
+			}
+		}
+
+		int print_valid (request& x, FILE *fp = stdout) const
+		{
+			int count = 0;
+			for (auto *curr = head ; curr ; curr = curr->next)
+				if (x.apply(*curr))
+				{
+					curr->print(fp, x.order.get());
+					++count;
+				}
+
+			fprintf(fp, "\n");
+			return count;
+		}
+
+		int get_length () const
+		{
+			int len = 0;
+			for (list2_node<T> *curr = head ; curr ; len += curr->get_length(), curr = curr->next);
+			return len;
+		}
+
+		int operator< (const list2 & b) const
+		{
+			if (!b.head)
+				return 0;
+			if (!head)
+				return 1;
+			
+			if (*head < *b.head)
+				return 1;
+
+			return 0;
+		}
+	private:
+		void erase ()
+		{
+			list2_node<T> *next = nullptr;
+			for (list2_node<T> *curr = head ; curr ; curr = next)
+			{
+				next = curr->next;
+				delete curr;
+			}
+
+			head = nullptr;
+		}
+};
+
+#endif // LIST_H
