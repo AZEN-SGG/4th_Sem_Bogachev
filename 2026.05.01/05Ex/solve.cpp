289f7787a714da237a85a9a01309fb133f8cf357
AZEN-SGG <rascatov.alex@gmail.com>
2026-05-01 10:30:25 +0300

    worked

---
+++ 2026.05.01/05Ex/solve.cpp
@@ -0,0 +1,77 @@
+#include "solve.h"
+#include "io_status.h"
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <utility>
+
+io_status read (FILE *in, words_t& lines)
+{
+	std::string buf;
+	char BUF[LEN] = {};
+	
+	for (int num = 1 ; fgets(BUF, LEN, in) ; ++num)
+	{
+		buf = BUF;
+		if ((!buf.empty()) && buf.back() == '\n')
+			buf.pop_back();
+		lines.emplace_back(std::move(buf));
+	}
+
+	return io_status::success;
+}
+
+int number_contains (FILE *in, FILE *out, words_t& words, const char *t)
+{
+	std::sort(words.begin(), words.end());
+	words.erase(std::unique(words.begin(), words.end()), words.end());
+
+	int len_common = 0;
+	std::string temp;
+	char BUF[LEN] = {},
+		 *saveptr = nullptr;
+
+	while (fgets(BUF, LEN, in))
+	{
+		int len_contains = 0,
+			len = 0;
+
+		temp = BUF;
+		len = temp.length();
+
+		if (BUF[len - 1] == '\n')
+			temp[len - 1] = BUF[len - 1] = '\0';
+
+		for (char *str = BUF ; (str = strtok_r(str, t, &saveptr)) ; str = nullptr)
+			len_contains += std::binary_search(words.begin(), words.end(), str);
+		
+		fprintf(out, "%d %s\n", len_contains, temp.c_str());
+		len_common += len_contains;
+	}
+
+	return len_common;
+}
+
+io_status t5_solve (const char *a, const char *b, const char *f_out, const char *t, int& r)
+{
+	io_status ret;
+	words_t words;
+
+	ret = read_file(a, words);
+	if (ret != io_status::success)
+		return ret;
+
+	FILE *in = fopen(b, "r");
+	if (!in)
+		return io_status::open;
+
+	FILE *out = fopen(f_out, "w");
+	if (!out)
+		return io_status::open;
+
+	r = number_contains(in, out, words, t);
+
+	fclose(in);
+	fclose(out);
+	return io_status::success;
+}
